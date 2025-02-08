3090ff4236e74f3f9d761efee4fdfbbf78b90d86
Renegade Data <renegade@renegadedata.space>
2025-02-08 17:00:14 +0000

    added AAS_Jan2022

---
+++ copag/meetings/AAS_Jan2022/AAS2022-Agenda-IRSTIG.php
@@ -0,0 +1,438 @@
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+<title>Cosmic Origins (COR)</title>
+<!-- Meta -->
+	<meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="name" content="Cosmic Origins (COR)">
+	<meta name="description" content="Web site for the NASA Cosmic Origins Program">
+	<meta name="keywords" content="cosmic origins, cosmic, origin, astrophysics, universe, universe formation, dark matter, galaxies, galaxy evolution, stars, star formation, planet formation, ultraviolet, telescope, far-infrared, interstellar medium, nasa">
+	<meta name="robots" content="noodp">
+	<meta name="doc-class" content="Living Document" />
+
+<!-- NASA Required Meta -->
+	<meta name="orgcode" content="660">
+    <meta name="rno" content="Peter Kurczynski">
+	<meta name="content-owner" content="Peter Kurczynski">
+	<meta name="webmaster" content="Stephan.F.Fantasia.1">
+
+<!-- Mobile Specific Meta --><!-- Pat Added second two lines -->
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
+
+<!-- Favicon -->
+	<link rel="shortcut icon" href="/modern/inc/img/favicon.png">
+
+<!-- CSS -->
+    <link rel="stylesheet" href="/modern/inc/css/details.css">
+	<link rel="stylesheet" href="/modern/inc/css/cosmic.css">
+	<link rel="stylesheet" href="/modern/inc/css/default2.css">
+    <link rel="stylesheet" href="/modern/inc/css/probes-cards.css">
+    <link rel="stylesheet" href="/modern/inc/css/news-scroll.css">
+
+
+<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA&subagency=GSFC" id="_fed_an_ua_tag">
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Javascript -->
+	<!-- Load html5shiv for old IE browsers -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!-- Google Analytics -->
+	<!--#include virtual="/jdm_modern/inc/html/analytics.html"-->
+
+</head>
+
+<body>
+<div id="skip"><a href="#content">Skip navigation and go straight to page content.</a></div> <!-- Skip To Content -->
+
+<header> <!-- Start NASA Header -->
+<div id="header-container"> <!-- Start Header Container -->
+<!-- NASA Logo -->
+	<div class="nasa-logo"><a href="https://www.nasa.gov"><img src="/modern/inc/img/NASA_logo.png" alt="NASA Logo"></a></div>
+<!-- Navigation (Hidden on Mobile/Tablet)-->
+	<div id="topnav">
+    <nav> <!-- COR Start Site Navigation -->
+
+<ul>	<li><a href="/">Home</a>	<ul>
+        <li><a href="/program/program-office.php" title="Cosmic Origins Program Office">Program Office</a></li>
+		<li><a href="/projects/current.php">Projects</a></li>
+		<li><a href="https://apd440.gsfc.nasa.gov/team.html" target="_blank">Project Office</a></li>
+	</ul></li>
+	<li><a href="/copag/community.php">Community</a>
+	<ul>		<li><a href="/copag/ec/copag-ec.php">Executive Committee</a></li>
+		<li><a href="/copag/science-gaps/science-gaps.php">Precursor Science Gaps</a></li>
+		<li><a href="/copag/program/cosmic-pathfinders.php">Cosmic Pathfinders</a></li>
+		<li><a href="/copag/meetings/meetings.php">Meetings</a></li>
+		<li><a href="/copag/events-calendar.php">Events Calendar</a></li>
+		<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSct2xrDo2SdKWqAsYSSNHgMHnVJQ2YWuIoFhiLAi85Wl4sDIg/viewform">Submit an Announcement</a></li>
+	</ul></li>
+
+    <li><a href="/sigs/sigs.php">Science Interest &amp; Analysis Groups</a>
+        <ul>
+            <li><a href="/sigs/agnsig.php">AGN SIG</a></li>
+            <li><a href="/sigs/dgce-sig.php">Cosmic Ecosystems SIG</a></li>
+            <li><a href="/sigs/galaxies-sig.php">Galaxies SIG</a></li>
+            <li><a href="/stigs/irstig.php">InfraRed STIG</a></li>
+            <li><a href="/sigs/starssig.php">Stars SIG</a></li>
+            <li><a href="/sigs/tdamm-sig.php">TDAMM SIG</a></li>
+            <li><a href="/stigs/uvstig.php">UV STIG / QUEST</a></li>
+            <li><a href="/sags/ngo-sag.php">New Great Observatories SAG</a></li>
+            <li><a href="/sags/Completed-SAGs.php">Completed SAGs</a></li>
+        </ul>
+    </li>
+
+	<li><a href="/studies/studies.php">Mission Studies</a>
+    <ul>
+		<li><a href="/studies/habitable-worlds/hwo.php">Habitable Worlds Observatory (HWO)</a></li>
+		<li><a href="/Astro2020/Astro2020.php">Astro2020 Decadal Survey</a></li>
+        <li><a href="https://science.nasa.gov/about-us/large-mission-study" target="_blank">SMD Large Mission Study</a></li>
+        <li><a href="/studies/probe-study/probe-study.php">2016 Probe Class Mission Study</a></li>
+        <li><a href="/studies/rfi/COPAG-Large-Missions.php">2015 COPAG Large Mission Study</a></li>
+        <li><a href="/copag/RFI2012/RFI2012.php">2012 UV / Visible Astrophysics Mission Concepts</a></li>
+	</ul>
+    </li>
+
+	<li><span><a name="Resources">Resources</a></span>
+    <ul>
+        <li><a href="https://apd440.gsfc.nasa.gov/technology/" target="_blank">Astrophysics Technology Development</a></li>
+        <li><a href="http://www.astrostrategictech.us/" target="_blank">Technology Database</a></li>
+		<li><a href="/resources/Research_Opportunities.php">Research Opportunities</a></li>
+        <li><a href="/resources/Conferences-Workshops.php">Conferences and Workshops</a></li>
+		<li><a href="https://nspires.nasaprs.com/external/" target="_blank">NSPIRES</a></li>
+        <li><a href="/science/career.php">NASA Career Opportunities</a></li>
+		<li><a href="https://heasarc.gsfc.nasa.gov/" target="_blank">HEASARC Data Archive</a></li>
+	</ul>
+    </li>
+
+
+
+</ul>
+
+</nav> <!-- End Site Navigation -->
+	</div>
+<!-- Mobile/Tablet Menu Access (Hidden on Desktop)-->
+	<div id="mobile-nav-icon">
+		<label for="menu-toggle" class="label-toggle"><img src="/modern/inc/img/hamburger_menu_icon.png" alt="Menu"></label>
+		<input id="menu-toggle" type="checkbox">
+    <nav> <!-- COR Start Site Navigation -->
+
+<ul>	<li><a href="/">Home</a>	<ul>
+        <li><a href="/program/program-office.php" title="Cosmic Origins Program Office">Program Office</a></li>
+		<li><a href="/projects/current.php">Projects</a></li>
+		<li><a href="https://apd440.gsfc.nasa.gov/team.html" target="_blank">Project Office</a></li>
+	</ul></li>
+	<li><a href="/copag/community.php">Community</a>
+	<ul>		<li><a href="/copag/ec/copag-ec.php">Executive Committee</a></li>
+		<li><a href="/copag/science-gaps/science-gaps.php">Precursor Science Gaps</a></li>
+		<li><a href="/copag/program/cosmic-pathfinders.php">Cosmic Pathfinders</a></li>
+		<li><a href="/copag/meetings/meetings.php">Meetings</a></li>
+		<li><a href="/copag/events-calendar.php">Events Calendar</a></li>
+		<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSct2xrDo2SdKWqAsYSSNHgMHnVJQ2YWuIoFhiLAi85Wl4sDIg/viewform">Submit an Announcement</a></li>
+	</ul></li>
+
+    <li><a href="/sigs/sigs.php">Science Interest &amp; Analysis Groups</a>
+        <ul>
+            <li><a href="/sigs/agnsig.php">AGN SIG</a></li>
+            <li><a href="/sigs/dgce-sig.php">Cosmic Ecosystems SIG</a></li>
+            <li><a href="/sigs/galaxies-sig.php">Galaxies SIG</a></li>
+            <li><a href="/stigs/irstig.php">InfraRed STIG</a></li>
+            <li><a href="/sigs/starssig.php">Stars SIG</a></li>
+            <li><a href="/sigs/tdamm-sig.php">TDAMM SIG</a></li>
+            <li><a href="/stigs/uvstig.php">UV STIG / QUEST</a></li>
+            <li><a href="/sags/ngo-sag.php">New Great Observatories SAG</a></li>
+            <li><a href="/sags/Completed-SAGs.php">Completed SAGs</a></li>
+        </ul>
+    </li>
+
+	<li><a href="/studies/studies.php">Mission Studies</a>
+    <ul>
+		<li><a href="/studies/habitable-worlds/hwo.php">Habitable Worlds Observatory (HWO)</a></li>
+		<li><a href="/Astro2020/Astro2020.php">Astro2020 Decadal Survey</a></li>
+        <li><a href="https://science.nasa.gov/about-us/large-mission-study" target="_blank">SMD Large Mission Study</a></li>
+        <li><a href="/studies/probe-study/probe-study.php">2016 Probe Class Mission Study</a></li>
+        <li><a href="/studies/rfi/COPAG-Large-Missions.php">2015 COPAG Large Mission Study</a></li>
+        <li><a href="/copag/RFI2012/RFI2012.php">2012 UV / Visible Astrophysics Mission Concepts</a></li>
+	</ul>
+    </li>
+
+	<li><span><a name="Resources">Resources</a></span>
+    <ul>
+        <li><a href="https://apd440.gsfc.nasa.gov/technology/" target="_blank">Astrophysics Technology Development</a></li>
+        <li><a href="http://www.astrostrategictech.us/" target="_blank">Technology Database</a></li>
+		<li><a href="/resources/Research_Opportunities.php">Research Opportunities</a></li>
+        <li><a href="/resources/Conferences-Workshops.php">Conferences and Workshops</a></li>
+		<li><a href="https://nspires.nasaprs.com/external/" target="_blank">NSPIRES</a></li>
+        <li><a href="/science/career.php">NASA Career Opportunities</a></li>
+		<li><a href="https://heasarc.gsfc.nasa.gov/" target="_blank">HEASARC Data Archive</a></li>
+	</ul>
+    </li>
+
+
+
+</ul>
+
+</nav> <!-- End Site Navigation -->
+	</div>
+
+</div> <!-- End Header Container -->
+</header> <!-- End NASA Header -->
+
+<div id="site-banner"> <!-- Start Site Banner -->
+	<div id="site-title">Cosmic Origins</div>
+	<div id="site-subtitle">Searching for answers about our universe and its origins</div>
+</div> <!-- End Site Banner -->
+
+
+
+
+<div id="site-container"> <!-- Start Site Container -->
+
+<div id="page-wrapper"> <!-- Start Page Wrapper -->
+
+
+
+
+<div id="left-column">
+
+
+<h1>Cosmic Origins Program Analysis Group<br>
+Events: Meetings</h1>
+
+
+<h2><a href="https://aas.org/meetings/aas239">239th Meeting of the American Astronomical Society</a><br>
+(<span style="color: red; font-weight: bold">Cancelled</span>)</h2>
+
+<p>Salt Lake City, Utah<br>
+9 &#8211; 13 January 2022</p>
+
+<hr>
+
+<h3 class="spaced-top">NASA Infrared Science &amp; Technology Interest Group (IR STIG)</h3>
+<p>Tuesday, 4 January 2022<br>
+1:00pm &#8211; 3:00pm ET<br>
+<strong>Webinar</strong></p>
+
+<details class="agenda" open><summary><h4>Agenda</h4></summary>
+
+
+<table>
+<thead>
+    <tr>
+        <th>Time</th>
+        <th>Event</th>
+        <th>Speaker</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>1:00pm</td>
+        <td>Introduction and Welcome [<a href="/copag/meetings/AAS_Jan2022/presentations/HostSlides-IRSTIG.pdf" target="_blank">PDF</a>]</td>
+        <td>IR STIG Host</td>
+    </tr>
+
+    <tr>
+        <td>1:05pm</td>
+        <td>Impressions of the Decadal Review [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Rachel_Osten.pdf" target="_blank">PDF</a>]</td>
+        <td>Rachel Osten</td>
+    </tr>
+
+    <tr>
+        <td>1:15pm</td>
+        <td>The Astrophysics Stratospheric Telescope for High Spectral Resolution Observations at Submillimeter-wavelengths, ASTHROS [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Jorge_Pineda.pdf" target="_blank">PDF</a>]</td>
+        <td>Jorge L. Pineda</td>
+    </tr>
+
+    <tr>
+        <td>1:23pm</td><td>An Update on the Experiment  for Cryogenic Large- Aperture Intensity Mapping  (EXCLAIM): an instrument advancing line-intensity-mapping techniques, and the use of KID-based microwave spectrometers. [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Carolyn_Volpert.pdf" target="_blank">PDF</a>]</td>
+        <td>Carrie Volpert</td>
+    </tr>
+
+    <tr>
+        <td>1:31pm</td>
+        <td>Science, Required Technology, and Scope of a Probe-Class Far-Infrared Observatory [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Jason_Glenn.pdf" target="_blank">PDF</a>]</td>
+        <td>Jason Glenn</td>
+    </tr>
+
+    <tr>
+        <td>1:39pm</td>
+        <td>The Cosmic Infrared Background ExpeRiment-2: First Flight Status Report [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Serena_Tramm.pdf" target="_blank">PDF</a>]</td>
+        <td>Serena Tramm</td>
+    </tr>
+
+    <tr>
+        <td>1:47pm</td>
+        <td>SOFIA Supporting the IR/FIR Community [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Margaret_Meixner.pdf" target="_blank">PDF</a>]</td>
+        <td>Margaret Meixner</td>
+    </tr>
+
+    <tr>
+        <td>1:55pm</td>
+        <td>The Near Earth Object Surveyor [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Edward_Wright.pdf" target="_blank">PDF</a>]</td>
+        <td>Edward L Wright</td>
+    </tr>
+
+    <tr>
+        <td>2:03pm</td>
+        <td>The Leighton Chajantor Telescope</td>
+        <td>Sunil Golwala</td>
+    </tr>
+
+    <tr>
+        <td>2:11pm</td><td>IRB: The Infrared Balloon Payload for High-Accuracy, High-Precision Photometry [<a href="/copag/meetings/AAS_Jan2022/presentations/Kraemer_IRB2022.pdf" target="_blank">PDF</a>]</td>
+        <td>Kathleen Kraemer</td>
+    </tr>
+
+    <tr>
+        <td>2:19pm</td>
+        <td>odeling Ionized Gas at Low Metallicities: The Wolf-Rayet Emission Nebula N76 [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Elizabeth_Tarantino.pdf" target="_blank">PDF</a>]</td>
+        <td>Elizabeth Tarantino</td>
+    </tr>
+
+    <tr>
+        <td>2:27pm</td>
+        <td>Orbiting Astronomical Telescope for Investigating Stellar Systems: OASIS</td>
+        <td>Christopher K. Walker</td>
+    </tr>
+
+    <tr>
+        <td>2:35pm</td>
+        <td>The GUSTO Balloon Mission [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Paul_Goldsmith.pdf" target="_blank">PDF</a>]</td>
+        <td>Paul Goldsmith</td>
+    </tr>
+
+    <tr>
+        <td>2:43pm</td>
+        <td>Millimetron Space Mission [<a href="/copag/meetings/AAS_Jan2022/presentations/IRSIG_Talk_Andrey_Baryshev.pdf" target="_blank">PDF</a>]</td>
+        <td>Andrey Baryshev</td>
+    </tr>
+
+    <tr>
+        <td>2:51pm</td>
+        <td>Open Discussion &amp; Adjourn</td>
+        <td>IR STIG Host</td>
+    </tr>
+</tbody>
+</table>
+
+</details>
+
+
+
+</div>
+
+
+<div id="right-column">
+
+
+
+
+<div class="panel">
+	<div class="panel-title">News</div>
+	<div class="panel-content">
+
+<!-- Panel Item Entry -->
+        <div class="pi-date">4 February 2025</div>
+		<div class="pi-title">Habitable Worlds Observatory (HWO) Seminar Series | February 12th, 2025 @ 1:00pm &#8211; 2:00pm ET / 10:00am &#8211; 11:00am PT</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/7_HWO_Seminar_Series.php" target="_blank">Details</a></div>
+
+<!-- Panel Item Entry -->
+        <div class="pi-date">30 January 2025</div>
+		<div class="pi-title">ROSES-24 Amendments to F.5 FINESST: SMD’s Graduate Student Research Opportunity</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/3_ROSES-24_Amendments_to_F.5_FINESST.php" target="_blank">Details</a></div>
+
+<!-- Panel Item Entry -->
+        		<div class="pi-title">Notice to Grant Awardees Regarding No-Cost Extensions</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/4_Notice_to_Grant_Awardees_with_No-Cost_Extensions.php" target="_blank">Details</a></div>
+
+<!-- Panel Item Entry -->
+        		<div class="pi-title">ROSES-2024 Amendment 102: Due Date Delays for Several Astrophysics Programs</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/5_ROSES-2024_Amendment_102.php" target="_blank">Details</a></div>
+
+<!-- Panel Item Entry -->
+        		<div class="pi-title">DEADLINE EXTENDED: Ultraviolet Science &amp; Instrumentation: On The Way To Habitable Worlds Observatory and Beyond (JATIS Special Issue) | March 7th , 2025</div>
+		<div class="pi-text"> &raquo;&nbsp; <a href="/news/2025/6_JATIS_Special_Issue_Submissions_Deadline_Extended.php" target="_blank">Details</a></div>
+
+<div class="flexMenu">
+    <div class="MenuItem readRight"><a href="/news/news-archive.php">News Archive</a></div>
+</div>
+
+<hr>
+
+<div class="flexMenu">
+    <div class="MenuItem readRight"><a href="/cor-news-email-list.php">Subscribe to COR News</a></div>
+</div>
+
+
+    </div>
+</div>
+
+
+</div>
+
+
+
+</div> <!-- End Page Wrapper -->
+<hr>
+</div> <!-- End Site Container -->
+
+<!-- Start Footer -->
+<footer>
+<div id="footer-container"> <!-- Start Footer Container -->
+<!-- NASA Logo -->
+	<div class="nasa-logo">
+	<a href="https://www.nasa.gov"><img src="/modern/inc/img/NASA_logo.png" width="60" alt="NASA Logo"></a>
+	</div>
+<!-- Contacts -->
+	<div id="contacts">
+	<ul>
+		<li>Curator: <a href="&#109;&#097;&#105;&#108;&#116;&#111;:patricia.a.tyler@nasa.gov">Pat Tyler</a></li>
+		<li>NASA Official: <a href="mailto:peter.kurczynski@nasa.gov">Peter Kurczynski</a></li>
+	</ul>
+	</div>
+<!-- Notices -->
+	<div id="notices">
+	<ul>
+		<li><a href="https://www.nasa.gov/about/highlights/HP_Privacy.html">Privacy Policy and Important Notices</a></li>
+		<li><a href="https://www.nasa.gov/about/contact/index.html">Contact NASA</a></li>
+			</ul>
+	</div>
+</div> <!-- End Footer Container -->
+</footer>
+<!-- End Footer -->
+
+</body>
+</html>
